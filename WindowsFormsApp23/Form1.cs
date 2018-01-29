c5fe21aa02c0982602dedef6fe3565fcf899aa41
Qemer Akhundova <qemer.akhund@hotmail.com>
2018-01-29 21:49:03 +0400

    Add project files.

---
+++ WindowsFormsApp23/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int top = 10;
+            int num = 0;
+           for(int i = 0; i < 5; i++)
+            {
+             int left = 80;
+
+            for(int j = 0; j < 5; j++)
+            {
+
+              Button btn = new Button();
+
+                    btn.Width = 30;
+                    btn.Height = 25;
+                    btn.Left = left;
+                    btn.Top = top;
+                    btn.Visible = true;
+                    btn.BackColor = SystemColors.Control;
+                    Controls.Add(btn);
+                    num++;
+
+                    btn.Text = num.ToString();
+                    btn.Click += new EventHandler(btn_Click);
+                    Form form = new Form();
+                    
+                    left += 50;
+            }
+                    top += 30;
+
+            }
+        }
+       
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.BackColor == SystemColors.Control)
+            {
+                button.BackColor = Color.Red;
+            }
+            else if (button.BackColor == Color.Green)
+            {
+                button.BackColor = Color.Green;
+            }
+            else
+            {
+                button.BackColor = SystemColors.Control;
+            }
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var listSelected = Controls.OfType<Button>().Where(x => x.BackColor == Color.Red);
+
+            foreach (Button item in listSelected)
+            {
+                item.BackColor = Color.Green;
+            }
+            
+        }
+    }
+}
---
+++ WindowsFormsApp23/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp23
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Beyin ogurlanmasi");
+            comboBox1.Items.Add("Arshin mal alan");
+            comboBox1.Items.Add("Alma almaya benzer");
+        }
+    }
+}
